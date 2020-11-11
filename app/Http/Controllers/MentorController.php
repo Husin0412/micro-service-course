430a4f7d4c32bb168a46714946f130e07d710611
Husin <Muhamad.husin0412@gmail.com>
2020-11-11 14:01:50 +0700

    chapter,mentor,course|api

--- app/Http/Controllers/MentorController.php
+++ app/Http/Controllers/MentorController.php
@@ -8,6 +8,35 @@ use Illuminate\Support\Facades\Validator;
 
 class MentorController extends Controller
 {
+
+    public function index()
+    {
+        $mentors = Mentor::all();
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentors,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $mentor = Mentor::find($id);
+        // $m = \json_decode(\json_encode($mentor), true);
+        // $count = $m ? count($m) : 0;
+        if (!$mentor) {
+            return \response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $mentor,
+        ]);
+
+    }
+
     public function create(Request $request)
     {
         $rules = [
@@ -36,4 +65,61 @@ class MentorController extends Controller
         ]);
 
     }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'required|string',
+            'profile' => 'required|url',
+            'profession' => 'required|string',
+            'email' => 'required|email',
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return \response()->json([
+                'status' => 'error',
+                'message' => $validator->errors(),
+            ], 400);
+        }
+
+        $mentor = Mentor::find($id);
+
+        if (!$mentor) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found',
+            ], 404);
+        }
+
+        $mentor->fill($data);
+
+        $mentor->save();
+        return \response()->json([
+            'status' => 'success',
+            'data' => $mentor,
+        ], 200);
+
+    }
+
+    public function destroy($id)
+    {
+        $mentor = Mentor::find($id);
+        if (!$mentor) {
+            return \response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found',
+            ], 404);
+        }
+
+        $mentor->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'mentor deleted',
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 // use Illuminate\Http\Request;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\MentorController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +20,15 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+// mentor
+Route::get('mentors', [MentorController::class, 'index']);
+Route::get('mentors/{id}', [MentorController::class, 'show']);
 Route::post('mentors', [MentorController::class, 'create']);
+Route::put('mentors/{id}', [MentorController::class, 'update']);
+Route::delete('mentors/{id}', [MentorController::class, 'destroy']);
+
+// course
+Route::get('courses', [CourseController::class, 'index']);
+Route::post('courses', [CourseController::class, 'create']);
+Route::put('courses/{id}', [CourseController::class, 'update']);
+Route::delete('courses/{id}', [CourseController::class, 'destroy']);
